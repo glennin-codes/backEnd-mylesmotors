edc4fa17c1f308a9c0a3c5ee6d790b67811c7e84
amschel99 <6290.2020@students.ku.ac.ke>
2022-10-24 16:43:54 +0300

    Create: add the required packages

---
+++ models/cars.js
@@ -0,0 +1,89 @@
+const mongoose= require("mongoose")
+const carsSchema= new mongoose.Schema(
+    {
+        carId:{
+            type:String,
+            required:[true,"name must be provided please"],
+            trim:true
+      
+        },
+        carName:{
+            type:String,
+            required:[true,"name must be provided please"],
+            trim:true
+      
+        },
+
+        carType:{
+            type:String,
+            required:[true, "car type must be provided"]
+        },
+        
+        carImage:{
+            type:String,
+            required:[true, " a product must have an Image"]
+        },
+       
+        image2:{
+            type:String,
+        },
+       
+        image3:{
+            type:String,
+        },
+       
+        image4:{
+            type:String,
+        },
+       
+        image5:{
+            type:String,
+        },
+        image6:{
+            type:String,
+        },
+        image7:{
+            type:String,
+        },
+        image8:{
+            type:String,
+        },
+        image9:{
+            type:String,
+        },
+        image10:{
+            type:String,
+        },
+        color: {
+            type: String,
+        },
+
+        transmission:{
+            type: String,
+            required:[true, "specify the transimission"]
+        },
+        
+        fuel:{
+        type:String,
+         
+        },
+        bodyStyle:{
+            type:String,
+
+        },
+        engine:{
+            type:String,
+        },
+        mileAge:{
+            type:String,
+        },
+        cubicCentimeters:{
+            type: String,
+            required: true
+        },
+        price:{
+            type:Number,
+        }
+    }
+)
+module.exports= mongoose.model("CarsModel",carsSchema  )
